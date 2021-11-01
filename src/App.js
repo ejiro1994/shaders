7b64e457bb9e2979e9b1d98bb792cef3fe06d990
Ejiro Akpoduado <eau01@ukc02qf3etgg7l.bskyb.com>
2021-11-01 17:12:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import * as THREE from 'three'
+import React, { useRef, Suspense } from "react"
+import { Canvas, extend, useFrame } from "@react-three/fiber"
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro"
 import './App.css';
 
-function App() {
+const WaveShaderMaterial = shaderMaterial(
+  // Uniforms
+  { uTime: 0, uColor: new THREE.Color(0, 0, 0) },
+  // vertex shaders
+  glsl`
+
+    varying vec2 vUv;
+
+    void main() {
+
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4
+      (position, 1.0);
+    }
+  `,
+  // Fragment Shader
+  glsl`
+
+  precision mediump float; 
+
+  uniform vec3 uColor;
+  uniform float uTime;
+
+  varying vec2 vUv;
+
+  void main() {
+    gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 1.0);
+  }
+  
+  `,
+)
+
+extend({ WaveShaderMaterial })
+
+const Wave = () => {
+const ref = useRef();
+useFrame(({clock}) => (ref.current.uTime = clock.getDelta.getElapsedTime()) )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh>
+      <planeBufferGeometry args={[3, 5]} />
+      <waveShaderMaterial uColor="gold" />
+    </mesh>
+  )
+}
+
+
+const Scene = () => {
+  return (
+    <Canvas>
+      {/* <pointLight position={[10, 10, 10]} /> */}
+      <Suspense fallback={null} >
+        <Wave />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+const App = () => {
+  return <Scene />
 }
 
 export default App;
