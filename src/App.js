3e9024c0ec60c75a36c529b35a3420659996701e
ejiro <ejiro1994@gmail.com>
2021-11-02 22:20:04 +0000

    working texture image

--- src/App.js
+++ src/App.js
@@ -1,60 +1,83 @@
 import * as THREE from 'three'
 import React, { useRef, Suspense } from "react"
-import { Canvas, extend, useFrame } from "@react-three/fiber"
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber"
 import { shaderMaterial } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro"
 import './App.css';
 
+
+
 const WaveShaderMaterial = shaderMaterial(
   // Uniforms
-  { uTime: 0, uColor: new THREE.Color(0, 0, 0) },
+
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0, 0, 0),
+    uTexture: new THREE.Texture(),
+  },
   // vertex shaders
   glsl`
+  precision mediump float; 
 
     varying vec2 vUv;
+  uniform float uTime;
+
+  #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
 
     void main() {
 
       vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4
-      (position, 1.0);
+
+      vec3 pos = position;
+      float noiseFreq = 1.5;
+      float noiseAmp = 0.25;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
     }
   `,
   // Fragment Shader
   glsl`
 
-  precision mediump float; 
+    precision mediump float;
 
   uniform vec3 uColor;
-  uniform float uTime;
+    uniform float uTime;
+    uniform sampler2D uTexture;
 
   varying vec2 vUv;
 
   void main() {
-    gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 1.0);
+    vec3 texture = texture2D(uTexture, vUv).rgb;
+    gl_FragColor = vec4(texture, 1.0);
   }
   
-  `,
-)
+  `
+);
 
 extend({ WaveShaderMaterial })
 
 const Wave = () => {
 const ref = useRef();
-useFrame(({clock}) => (ref.current.uTime = clock.getDelta.getElapsedTime()) )
+useFrame(({clock}) => (ref.current.uTime = clock.getElapsedTime()) );
+
+  const [image] = useLoader(THREE.TextureLoader, [
+    "https://images.unsplash.com/photo-1604011092346-0b4346ed714e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+  ]);
 
   return (
     <mesh>
-      <planeBufferGeometry args={[3, 5]} />
-      <waveShaderMaterial uColor="gold" />
+      <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
+      <waveShaderMaterial uColor="gold" ref={ref} uTexture={image} />
+      {/* <waveShaderMaterial uColor={"hotpink"} ref={ref} uTexture={image} /> */}
     </mesh>
-  )
+  );
 }
 
 
 const Scene = () => {
   return (
-    <Canvas>
+    <Canvas camera={{fov: 9}}>
       {/* <pointLight position={[10, 10, 10]} /> */}
       <Suspense fallback={null} >
         <Wave />
